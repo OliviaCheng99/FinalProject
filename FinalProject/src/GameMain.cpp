00869cf92117ec8290d483dc647a937a9937980f
ricardo2892 <ricardo.sanchez.2830@gmail.com>
2022-09-21 10:49:36 -0600

    Final Project

---
+++ FinalProject/src/GameMain.cpp
@@ -0,0 +1,16 @@
+// MDS PROGRAM - FINAL PROJECT
+// SEPTEMBER 2022
+//
+// RICARDO SANCHEZ
+// OLIVIA CHENG
+//
+// CREATION OF XONIX GAME
+
+
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+int main() {
+    return 0;
+}
+
---
+++ FinalProject/xcode/CMakeFiles/3.24.2/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ricardo2830/FinalProject/FinalProject/xcode/CMakeFiles/3.24.2/CompilerIdC/CMakeCCompilerId.c
