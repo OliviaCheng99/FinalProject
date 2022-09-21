0e24985042578e18a3994f32deadf6d13f3085b2
Manman Cheng <olivia.chengman@gmial.com>
2022-09-21 01:18:52 -0600

    update

--- xcode/Xonix.build/Debug/ALL_BUILD.build/Script-93AAEA4B85B59301CE18E2F6.sh
+++ xcode/Xonix.build/Debug/ALL_BUILD.build/Script-93AAEA4B85B59301CE18E2F6.sh
@@ -2,18 +2,18 @@
 set -e
 if test "$CONFIGURATION" = "Debug"; then :
   cd /Users/olivia/FinalProject/xcode
-  make -f /Users/olivia/FinalProject/xcode/CMakeScripts/ReRunCMake.make
+  echo Build\ all\ projects
 fi
 if test "$CONFIGURATION" = "Release"; then :
   cd /Users/olivia/FinalProject/xcode
-  make -f /Users/olivia/FinalProject/xcode/CMakeScripts/ReRunCMake.make
+  echo Build\ all\ projects
 fi
 if test "$CONFIGURATION" = "MinSizeRel"; then :
   cd /Users/olivia/FinalProject/xcode
-  make -f /Users/olivia/FinalProject/xcode/CMakeScripts/ReRunCMake.make
+  echo Build\ all\ projects
 fi
 if test "$CONFIGURATION" = "RelWithDebInfo"; then :
   cd /Users/olivia/FinalProject/xcode
-  make -f /Users/olivia/FinalProject/xcode/CMakeScripts/ReRunCMake.make
+  echo Build\ all\ projects
 fi
 
