3a4fa7829baea09cbdbc78ffe4f1a5ed50180344
ricardo2892 <ricardo.sanchez.2830@gmail.com>
2022-09-21 11:25:05 -0600

    Update src files

---
+++ FinalProject/src/GameFunctions.cpp
@@ -0,0 +1,8 @@
+//
+//  GameFunctions.cpp
+//  FinalProject
+//
+//  Created by Ricardo Sanchez on 9/21/22.
+//
+
+#include "GameFunctions.hpp"
---
+++ FinalProject/src/GameFunctions.hpp
@@ -0,0 +1,13 @@
+//
+//  GameFunctions.hpp
+//  FinalProject
+//
+//  Created by Ricardo Sanchez on 9/21/22.
+//
+
+#ifndef GameFunctions_hpp
+#define GameFunctions_hpp
+
+#include <stdio.h>
+
+#endif /* GameFunctions_hpp */
